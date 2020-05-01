d33dd8bfce99b703e5cfa10f45c96063249adb11
Tushar Goyal <tgoyal@tavisca.in>
2020-05-02 00:09:23 +0530

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using Problems.May;
+using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //day1
+            FirstBadVersion firstBadVersion = new FirstBadVersion();
+            firstBadVersion.Run();
+
+        }
+    }
+}
---
+++ Problems.May/FirstBadVersion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.May
+{
+    public class FirstBadVersion : VersionControl
+    {
+        //day1
+        /*
+             F F F T T
+             approach1: iterate and return first T
+             approach2: use binary search to find first T
+         */
+        public int Run()
+        {            
+            return FindBadVersion(0, 4);
+        }
+
+        private int FindBadVersion(int v, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class VersionControl
+    {
+        readonly bool[] versionFlags = new bool[] { false, false, false, true, true };
+        public bool IsBadVersion(int n)
+        {
+            return versionFlags[n - 1];
+        }
+    }
+}
