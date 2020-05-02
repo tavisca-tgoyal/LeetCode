fe2081a244f99a453fccaadb53fd4bc9a7494cad
Tushar Goyal <tgoyal@tavisca.in>
2020-05-02 22:35:33 +0530

    FirstBadVersion Code done.

--- Problems.May/FirstBadVersion.cs
+++ Problems.May/FirstBadVersion.cs
@@ -13,19 +13,35 @@ namespace Problems.May
              approach2: use binary search to find first T
          */
         public int Run()
-        {            
-            return FindBadVersion(0, 4);
+        {
+            int length = versionFlags.Length;
+            return FindBadVersion(0, length);
         }
 
-        private int FindBadVersion(int v, int n)
+        private int FindBadVersion(int start, int end)
         {
-            throw new NotImplementedException();
+            //int mid = (start + end) / 2;
+            //do not use about expression to calculate mid, this will cause overflow in case of big inputs
+            int mid = start + (end - start) / 2;
+            if (IsBadVersion(mid) && mid != 0 && !IsBadVersion(mid - 1))
+                return mid;
+            else
+            {
+                if (IsBadVersion(mid) == false)
+                {
+                    return FindBadVersion(mid + 1, end);
+                }
+                else
+                {
+                    return FindBadVersion(start, mid);
+                }
+            }            
         }
     }
 
     public class VersionControl
     {
-        readonly bool[] versionFlags = new bool[] { false, false, false, true, true };
+        public readonly bool[] versionFlags = new bool[] { false, false, false, true, true };
         public bool IsBadVersion(int n)
         {
             return versionFlags[n - 1];
