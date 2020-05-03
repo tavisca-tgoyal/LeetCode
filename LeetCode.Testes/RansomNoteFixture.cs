03497ede9e56c52b34ec23cf80370250e053f274
Tushar Goyal <tgoyal@tavisca.in>
2020-05-03 21:40:32 +0530

    finished ransomnotes

---
+++ LeetCode.Testes/RansomNoteFixture.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using LeetCode.Challenges.May;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class RansomNoteFixture
+    {
+        [Fact]
+        public static void Running_program()
+        {
+            RansomNote ransomNote = new RansomNote();
+            ransomNote.Run("aa","aab").Should().Be(true);
+        }
+    }
+}
---
+++ Problems.May/May/RansomNote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Challenges.May
+{
+    public class RansomNote
+    {
+        public bool Run(string rNote, string magazine)
+        {
+            int[] mCounter = new int[26];
+            foreach (char ch in magazine)
+            {
+                mCounter[ch % 97]++;
+            }
+            foreach (char ch in rNote)
+            {
+                if (mCounter[ch % 97] > 0)
+                    mCounter[ch % 97]--;
+                else
+                    return false;
+            }
+            return true;
+        }
+        
+    }
+}
