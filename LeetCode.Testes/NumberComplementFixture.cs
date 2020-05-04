c7662ed0f4d5d236f217e9c0b1072f1d7e26e9c3
Tushar Goyal <tgoyal@tavisca.in>
2020-05-04 21:18:37 +0530

    implemented  NumberComplement

---
+++ LeetCode.Testes/NumberComplementFixture.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using LeetCode.Challenges.May;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class NumberComplementFixture
+    {
+        [Fact]
+        public static void Program_should_convert_decimal_to_binary_complement_and_return_decimal()
+        {
+            NumberComplement numberComplement = new NumberComplement();
+            numberComplement.Run(5).Should().Be(2);
+            numberComplement.Run(1).Should().Be(0);
+        }
+    }
+}
---
+++ Problems.May/May/NumberComplement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Challenges.May
+{
+    public class NumberComplement
+    {
+        
+        public int Run(int num)
+        {
+            return ToDecimal(Complement(ToBinary(num)));
+        }
+
+        private int ToDecimal(List<int> list)
+        {
+            int result = 0;
+            list.Reverse();
+            for (int i = 0; i < list.Count; i++)
+            {
+                result += Convert.ToInt32(Math.Pow(2,i)) * list[i];                
+            }
+            return result;
+        }
+        
+        private List<int> Complement(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == 0)
+                    list[i] = 1;
+                else
+                    list[i] = 0;
+            }
+            return list;
+        }
+
+        private List<int> ToBinary(int num)
+        {
+            List<int> temp = new List<int>();
+            while(num != 0)
+            {
+                temp.Add(num % 2);
+                num = num / 2;
+            }
+            temp.Reverse();
+            return temp;
+        }
+    }
+}
