e4e495cb79cdcd1445c8166d2479d30926eedcf1
Tushar Goyal <tgoyal@tavisca.in>
2020-05-07 13:53:15 +0530

    done with ValidPalindrome

---
+++ LeetCode.Testes/ValidPalindromeFixture.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using RandomProblems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class ValidPalindromeFixture
+    {
+        [Fact]
+        public static void Run_Program()
+        {
+            ValidPalindrome validPalindrome = new ValidPalindrome();
+            validPalindrome.Run("A man, a plan, a canal: Panama").Should().Be(true);
+
+        }
+    }
+}
---
+++ RandomProblems/ValidPalindrome.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomProblems
+{
+    public class ValidPalindrome
+    {        
+        private bool IsAlphaNumeric(char ch)
+        {
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+                return true;
+            else
+                return false;
+        }
+        //, a plan, a canal: Pa
+        public bool Run(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                char x = s[i];
+                char y = s[j];
+                if (!IsAlphaNumeric(s[i]))
+                {
+                    i++;
+                    continue;
+                }
+
+                if (!IsAlphaNumeric(s[j]))
+                {
+                    j--;
+                    continue;
+                }
+                if (!Compare(s[i],s[j]))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        private bool Compare(char v1, char v2)
+        {
+            if (ToUpper(v1).Equals(ToUpper(v2)))
+                return true;
+            else
+                return false;
+        }
+
+        private char ToUpper(char v1)
+        {
+            if (v1 >= 'A' && v1 <= 'Z')
+                return v1;
+            else
+            {
+                return Convert.ToChar(v1 - 32);
+            }
+        }
+    }
+}
